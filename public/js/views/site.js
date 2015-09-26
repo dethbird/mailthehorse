e3061a905ec7352e3e61cc9dbc308e8c3dfc838e
Rishi Satsangi <rishi.satsangi@gmail.com>
2015-09-26 12:00:21 -0400

    revision4

---
+++ public/js/views/site.js
@@ -0,0 +1,15 @@
+$(window).ready(function(){
+    $(window).resize($.debounce(350, function(e){
+      if ($(window).width() >= 992) {
+        $('section iframe.youtube').attr('width', 560);
+        $('section iframe.youtube').attr('height', 315);
+      } else if ($(window).width() >= 768 && $(window).width() < 992) {
+        $('section iframe.youtube').attr('width', 450);
+        $('section iframe.youtube').attr('height', 253);
+      } else if ($(window).width() < 768) {
+        $('section iframe.youtube').attr('width', 350);
+        $('section iframe.youtube').attr('height', 197);
+      }
+    }));
+    $(window).trigger('resize');
+});
